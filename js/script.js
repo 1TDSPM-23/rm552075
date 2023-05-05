5a893631cee474efb5385973491c972b471d9471
k-alm <kauan.alm.almeida@gmail.com>
2023-05-05 09:52:51 -0300

    Array JS

--- js/script.js
+++ js/script.js
@@ -26,30 +26,66 @@
 //    //OBTENHA O RESTO DA DIVISÃO
 //    console.log(`Resultado resto da divisão:  + ${parseInt(nr1) % parseInt(nr2)}`);
 
-   //DESCOBRINDO SE UM NÚMERO É PAR OU ÍMPAR
-   //UTILIZANDO OPERADOR TERNÁRIO (?)
-   var nr1 = "10";
-   var nr2 = "5";
-   var resultado;
+//    //DESCOBRINDO SE UM NÚMERO É PAR OU ÍMPAR
+//    //UTILIZANDO OPERADOR TERNÁRIO (?)
+//    var nr1 = "10";
+//    var nr2 = "5";
+//    var resultado;
 
-   resultado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "ÍMPAR";
-   console.log('O NÚMERO É: ${resultado}');
+//    resultado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "ÍMPAR";
+//    console.log('O NÚMERO É: ${resultado}');
 
-   //DIFERENÇA ENTRE VAR/LET/CONST
-//    var nome1 = "Beatriz";
-//    let nome2 = "Matheus";
-   let nome = "Beatriz";
+//    //DIFERENÇA ENTRE VAR/LET/CONST
+// //    var nome1 = "Beatriz";
+// //    let nome2 = "Matheus";
+//    let nome = "Beatriz";
 
-   if(nome == ""){
-    let nome = "Robson";
-   }
+//    if(nome == ""){
+//     let nome = "Robson";
+//    }
 
-   //Qual é o nome que será impresso no log?
-   console.log(nome);
+//    //Qual é o nome que será impresso no log?
+//    console.log(nome);
 
-   //REGRAS DO CONST
-   //1ª Regra = Inicializar na declaração
-   const nome3 = "Violante";
+//    //REGRAS DO CONST
+//    //1ª Regra = Inicializar na declaração
+//    const nome3 = "Violante";
 
-   //2ª Regra = Não pode ser alterado durante o fluxo do programa.
-   nome3 = "Jonathan";
+//    //2ª Regra = Não pode ser alterado durante o fluxo do programa.
+//    nome3 = "Jonathan";
+
+
+   //ARRAY e seus métodos
+   let frutas = ["banana", "maçã", "morango", "carambola", "acerola", "laranja"];
+
+   //Imprimindo um array
+   console.log(frutas);
+   console.table(frutas);
+   console.log(frutas[1]);
+
+    //Adicionar um iten ao final do array com o método push(iten);
+    frutas.push("melão");
+    console.log(frutas);
+    //Adicionar um iten no início do array com o método unshift(iten);
+    frutas.unshift("cajú");
+    console.log(frutas);
+    //Remover um iten do final do array com o método pop();
+    frutas.pop();
+    console.log(frutas);
+    //Remover um iten do início do array com o método shift();
+    frutas.shift();
+    console.log(frutas);
+
+    //Para localizar um item dentro do array, utilizamos o método indexOf (itemNome)
+    // let indice = frutas.indexOf("morango");
+    // console.log(frutas[indice]);    
+
+    //Para localizar um item dentro do array, e apagar ele utilizamos o método splice(indice do item, quantidade de vezes que o indice será removido).
+    //Obs: utilize o método indexOf(itemNome) para obter indice do item;
+    let indice = frutas.indexOf("carambola");
+    frutas.splice(indice, 1);
+    console.log(frutas)
+
+    frutas.forEach( (fruta)=>{} ){
+        console.log("fruta: " + fruta);
+    }
